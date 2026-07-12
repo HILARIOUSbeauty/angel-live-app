aa34340623800a37af909bad4f561b7f0804b964
HILARIOUSbeauty <poolvision8@gmail.com>
2026-07-13 00:05:25 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ============ ここだけ書き換えてください ============
 // Supabaseの Settings > API からコピー
 window.APP_CONFIG = {
-  SUPABASE_URL: "ここにProject URLを貼る",
-  SUPABASE_ANON_KEY: "ここにanon publicキーを貼る",
+  SUPABASE_URL: "https://pztsimnwikmuhqftlsib.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6dHNpbW53aWttdWhxZnRsc2liIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NTk1NzYsImV4cCI6MjA5OTQzNTU3Nn0.JTDGeDbmz2qVZXQ-Ur0AUeq3N4JqVxVCRreZDsfwTDc",
 };
